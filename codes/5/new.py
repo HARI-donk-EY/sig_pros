d46b7541df6c4dd89aaae7cb80915e45b54fddf0
HariVamshi6000 <ai21btech11014@iith.ac.in>
2022-09-22 10:25:49 +0530

    added new code in 5

---
+++ codes/5/new.py
@@ -0,0 +1,14 @@
+from sympy import Sum,var
+
+
+def usf(n):
+        return 1
+   
+
+def h(n):
+    return ((-0.5)**(n))*usf(n) + ((-0.5)**(n-2))*usf(n-2)
+
+var("n")
+expr1 = Sum(h(n),(n,0,float('inf')))
+print(expr1.doit())
+

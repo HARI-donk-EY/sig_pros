4e57b1f6b4f5d48e308fa124e20cb6d8f1890b6b
HariVamshi6000 <ai21btech11014@iith.ac.in>
2022-09-26 09:37:17 +0530

    started 6

---
+++ codes/6/XkHk.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+N = 20
+
+
+#print(X), (H)
+X = np.loadtxt('xkreal.dat')
+H = np.loadtxt('hkreal.dat')
+
+#subplots
+plt.subplot(2,1,1)
+plt.stem(range(0,N),X)
+plt.title('')
+plt.xlabel('$k$')
+plt.ylabel('$X(k)$')
+plt.grid()# minor
+
+plt.subplot(2,1,2)
+plt.stem(range(0,N),H)
+plt.title('')
+plt.xlabel('$k$')
+plt.ylabel('$H(k)$')
+plt.grid()# minor
+plt.savefig('../../figs/xkhk.pdf')
+plt.show()   
+
+

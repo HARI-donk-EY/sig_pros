bca6d79bb165ca5f53fda8b7b7a047a8b57d6c83
root <root@localhost.localdomain>
2022-10-15 09:02:32 +0000

    1.2

--- pingala/codes/1.py
+++ pingala/codes/1.py
@@ -25,15 +25,22 @@ vec_f1 = scipy.vectorize(f1)
 def f2(n, alpha, beta):
     return np.sum(vec_an(np.arange(n), alpha, beta))
 
+def f3(n, alpha, beta):
+    return np.dot(vec_an(np.arange(n), alpha, beta), np.array([1/(10**i) for i in range(n)]))-10/89
+
 vec_f2 = scipy.vectorize(f2)
+vec_f3 = scipy.vectorize(f3)
 
 alpha = (1 + np.sqrt(5))/2
 beta = (1 - np.sqrt(5))/2
 
 n = np.arange(1,100)
 
+# pblm 1.1
+
 l1 = vec_f1(n,alpha,beta)
 l2 = vec_f2(n,alpha,beta)
+l3 = vec_f3(n,alpha,beta)
 
 plt.subplot(211)
 plt.plot(n, l1, label=r'$a_{n+2}-1$', color = 'r')
@@ -46,3 +53,12 @@ plt.legend()
 plt.savefig('../figs/1_1.png')
 plt.show()
 
+# pblm 1.2
+
+
+
+plt.plot(n, l3, label=r'$\sum_{k=1}^{n}\frac{a_{k}}{10^k}-\frac{10}{89}$', color = 'r')
+plt.legend()
+plt.grid()
+plt.savefig('../figs/1_2.png')
+plt.show()

f7354054c860b818568795301958c6a5b032c5cb
root <root@localhost.localdomain>
2022-10-31 03:49:11 +0000

    t3st

--- pingala/exect.sh
+++
@@ -1,3 +0,0 @@
-texfot pdflatex main.tex
-texfot pdflatex main.tex
-rm main.aux main.out main.log main.toc
--- pingala/tex_exec.sh
+++ pingala/tex_exec.sh
@@ -1,3 +1,4 @@
 texfot pdflatex main.tex
 texfot pdflatex main.tex
 rm main.aux main.log main.out main.toc
+figlet "DONE"
